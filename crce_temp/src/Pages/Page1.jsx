0af3cb67367a5ffcf3f50f178033b1498ad4db52
japsare ayushi <japsareayushi@gmail.com>
2025-02-07 19:53:47 +0530

    Removed secret from HuggingFace.jsx

---
+++ crce_temp/src/Pages/Page1.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Page1 = () => {  
+    return(
+        <div>p1</div>
+    )
+};
+
+export default Page1;
---
+++ crce_temp/src/Pages/Page2.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Page2 = () => {  
+    return(
+        <div>p2</div>
+    )
+};
+
+export default Page2;
